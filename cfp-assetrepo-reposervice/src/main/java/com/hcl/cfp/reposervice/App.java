287560a44d50ae17124ab5b4950a207cdafe362f
ashish-sr <ashishsr@hcl.com>
2022-06-14 12:16:45 +0530

    Initial commit

---
+++ cfp-assetrepo-reposervice/src/main/java/com/hcl/cfp/reposervice/App.java
@@ -0,0 +1,13 @@
+package com.hcl.cfp.reposervice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cfp-assetrepo-reposervice/src/test/java/com/hcl/cfp/reposervice/AppTest.java
@@ -0,0 +1,12 @@
+package com.hcl.cfp.reposervice;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+  
+{
+
+}
